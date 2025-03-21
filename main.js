1b9226ca469c20e2ba9ba4a23affb01675478418
Hamza-Nawabi <111171188+Nawabi-Hamza@users.noreply.github.com>
2025-03-21 10:07:05 +0430

    update

--- main.js
+++ main.js
@@ -1,72 +1,19 @@
 const btn = document.querySelector(".click-button");
-// const icon = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-9Q0HbIRaLfEaEfsc-VrhnNMrAxvFddth6Q&s";
-const icon = "google-message-icon.png";
 
-if (btn) {
+const notifier = new NotificationManager();
+notifier.requestPermission();
+
+document.addEventListener("DOMContentLoaded", () => {
+    notifier.sendNotification("Hello Nawabi", "😊 Page Loaded Bro!");
     btn.addEventListener("click", () => {
-        pushMessage("Button Clicked", "This is a notification from Hamza Nawabi for testing", icon, "notify_me");
+        notifier.sendNotification("Hello Nawabi", "👆🏻 You clicked the button!");
     });
-}
+})
 
-document.addEventListener("visibilitychange", function () {
-    if (document.visibilityState === "hidden") {
-        setInterval(() => {
-            pushMessage("Please come back!", "We miss you!", icon, "notify_me");
-        }, 5000);
-    } else {
-        pushMessage("Welcome back!", "We are happy to see you again!", icon, "notify_me");
-    }
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    notifier.autoNotifyWhenHidden("Hello Nawabi", "🤔 You are still here?", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPNLA6Vl3xMbPDlgEMFb3QP04Xi0fpgIdfNQ&s", 30000);
+  }else{
+    notifier.sendNotification("Hello Nawabi", "👋🏻 Welcome back!");
+  }
 });
-
-let num = 0;
-async function pushMessage(title, body, icon, tag) {
-    num += 1;
-    console.log("Notification permission status:", Notification.permission);
-
-    const error = document.querySelector(".error");
-    const success = document.querySelector(".success");
-    const alert = document.querySelector(".alert-success");
-
-    if (error) error.textContent = ""; // Clear previous messages
-    if (success) success.textContent = "";
-    if (alert) alert.textContent = "";
-
-    try {
-        const permission = await Notification.requestPermission();
-        if (permission === "granted") {
-            if (success) success.textContent = "✅ Notification permission granted!";
-
-            const notification = new Notification(title, {
-                body: body,
-                icon: icon,
-                tag: tag,
-                // requireInteraction: true, // Keeps the notification open
-                renotify: true, // Resend notification if already there
-                // silent: true, // Sound alert
-                // vibrate: [200, 100, 200], // Vibrate alert
-                data: {
-                    url: "https://hamzanawabi.com",
-                    id: num,
-                },
-            });
-
-            notification.onclick = () => {
-                console.log("🟢 Notification clicked.");
-            };
-
-            notification.onclose = (e)=>{
-                console.log("🔴 Notification closed.");
-            }
-
-            notification.onerror = (e) => {
-                console.error("❌ Notification error:", e);
-            };
-
-        } else {
-            if (error) error.textContent = "❌ Notification permission denied!";
-        }
-    } catch (err) {
-        if (error) error.textContent = `❌ Error: ${err}`;
-        console.error("Error requesting notification permission:", err);
-    }
-}
