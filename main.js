c07e08d060370e60c195fedd034d62f685fc486e
Hamza-Nawabi <111171188+Nawabi-Hamza@users.noreply.github.com>
2025-03-21 10:47:30 +0430

    add url

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const notifier = new NotificationManager();
 notifier.requestPermission();
 
 document.addEventListener("DOMContentLoaded", () => {
-    notifier.sendNotification("Hello Nawabi", "😊 Page Loaded Bro!", "google-messages-icon.png");
+    notifier.sendNotification("Hello Nawabi", "😊 Page Loaded Bro!", "google-messages-icon.png", "Test-Notifcation","https://github.com/Nawabi-Hamza");
     btn.addEventListener("click", () => {
-        notifier.sendNotification("Hello Nawabi", "👆🏻 You clicked the button!", "google-messages-icon.png");
+        notifier.sendNotification("Hello Nawabi", "👆🏻 You clicked the button!", "google-messages-icon.png", "Test-Notifcation","https://github.com/Nawabi-Hamza");
     });
 })
 
-notifier.autoNotifyWhenHidden("Hello Nawabi", "🤔 You are still here?", "google-messages-icon-png", 30000);
+notifier.autoNotifyWhenHidden("Hello Nawabi", "🤔 You are still here?", "google-messages-icon-png", 30000, "https://github.com/Nawabi-Hamza");
